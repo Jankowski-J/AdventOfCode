abff95439a0d7541660333e0444ecf35c5c2d23a
Jakub Jankowski <jakub.jankowski@itmagination.com>
2017-12-07 09:34:57 +0100

    solution for day 2, part 2

--- 2017/Day2/script.js
+++ 2017/Day2/script.js
@@ -5,3 +5,15 @@ for (let arr of arrays) {
 	var diff = max - min; 
 	sum+=diff;
 }
+
+// PART 2
+
+function findDivisible(input) { 
+	for (let first of input) { 
+		for(let second of input) { 
+			if( first == second || first < second) continue;   
+			if(first % second == 0) return first / second; 
+		} 
+	} 
+}
+arrays.map(ar => findDivisible(ar)).reduce((prev, curr) => prev + curr, 0)
